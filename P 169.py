95aa74883ee9d95d018ca4ddbbcd2f8484c71a9e
DJ-kkk <129987327+DJ-kkk@users.noreply.github.com>
2024-07-09 17:07:46 +0100

    Add files via upload

---
+++ P 169.py
@@ -0,0 +1,17 @@
+from tkinter import 
+
+root = Tk()
+root.title("P 169")
+root.geometry("500x500")
+root.configure(background="lavenderblush")
+
+
+
+
+def createLabel(self):
+    
+class_btn = Button(root, text="Button", command = self.message)
+elemnt = dropdown.get()
+
+if(element == "Label"):
+    self.createLabel()
